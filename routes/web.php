3b885e5b02b8891c3eb0afe93e5d300378848723
Ricardo Montañana <rmontanana@gmail.com>
2018-06-25 16:53:34 +0200

    Añade middleware de confirmación de email a la creación de nuevo hilo. resolves #2

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/home', 'HomeController@index');
 Route::view('scan', 'scan');
 
 Route::get('threads', 'ThreadsController@index')->name('threads');
-Route::get('threads/create', 'ThreadsController@create');
+Route::get('threads/create', 'ThreadsController@create')->middleware('must-be-confirmed');
 Route::get('threads/search', 'SearchController@show');
 Route::get('threads/{channel}', 'ThreadsController@index');
 Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
